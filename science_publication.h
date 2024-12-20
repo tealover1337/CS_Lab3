7d03f385285589e5fb9a9f8b0d429744c18f017a
tealover1337 <vaniacherkasov@gmail.com>
2024-12-20 14:20:45 +0300

    added data structure (science publication)

---
+++ science_publication.h
@@ -0,0 +1,16 @@
+#ifndef SCIENCE_PUBLICATION_H
+#define SCIENCE_PUBLICATION_H
+
+typedef struct SciencePublication {
+    char name[127];
+    char author_surname[127];
+    char author_initials[63];
+    char magazine_name[127];
+    short year;
+    int volume;
+    bool rinc;
+    int pages;
+    int cites;    
+} SciencePublication;
+
+#endif
