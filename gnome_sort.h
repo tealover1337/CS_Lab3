39f09daef07822bb37cec989ea21ad3322361bef
tealover1337 <vaniacherkasov@gmail.com>
2024-12-28 13:57:13 +0300

    fixed bug

--- gnome_sort.h
+++ gnome_sort.h
@@ -2,11 +2,7 @@
 #define GNOME_SORT_H
 
 #include "stack.h"
-#include "science_publication.h"
-#include "comparator.h"
-#include <stdbool.h>
-#define T SciencePublication
 
-void gnome_sort(Stack* container, bool comparator(T elem1, T elem2), short type);
+void gnome_sort(Stack* container, short type);
 
 #endif
