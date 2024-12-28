3188b62d07d5e7390d24f65bda21769ddf200d4d
tealover1337 <vaniacherkasov@gmail.com>
2024-12-28 13:56:56 +0300

    fixed bug

--- gnome_sort.c
+++ gnome_sort.c
@@ -1,10 +1,11 @@
 #include "stack.h"
+#include "comparator.h"
 #include <stdbool.h>
 #include "science_publication.h"
 #define T SciencePublication
 
 // type = asc or dsc (0 or 1); ik I could persist dry principle but I'm too lazy to use boolean algebra basics
-void gnome_sort(Stack* container, bool comparator(T elem1, T elem2), short type) {
+void gnome_sort(Stack* container, short type) {
     size_t pos = 1;
     while (pos < get_size(container)) {
         if (pos == 0) {pos = 1;}
